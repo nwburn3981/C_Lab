91c2ca38ff869f6ac66c1bf89d189ca848ae0782
Nicholas Burns <42648266+nwburn3981@users.noreply.github.com>
2022-11-21 15:10:21 -0500

    Initial commit

---
+++ EFCore_Activity0801/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using EFCore_DBLibrary;
+using InventoryHelpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace EFCore_Activity0801
+{
+    class Program
+    {
+        private static IConfigurationRoot _configuration;
+        private static DbContextOptionsBuilder<AdventureWorksContext> _optionsBuilder;
+
+        static void Main(string[] args)
+        {
+            BuildOptions();
+            Console.WriteLine("List People Then Order And Take");
+            ListPeopleThenOrderAndTake();
+            Console.WriteLine("Query People, order, then list and take");
+            QueryPeopleOrderedToListAndTake();
+        }
+
+        private static void ListPeopleThenOrderAndTake()
+        {
+            using (var db = AdventureWorksContext(_optionsBuilder.Options))
+            {
+                var people = db.People.ToList().OrderByDescending(x => x.LastName);
+                foreach (var person in people.Take(10))
+                {
+                    Console.WriteLine($"{person.FirstName} {person.LastName}");
+                }
+            }
+        }
+
+        private static void QueryPeopleOrderedToListAndTake()
+        {
+            using (var db = AdventureWorksContext(_optionsBuilder.Options))
+            {
+                var query = db.People.OrderByDescending(x => x.LastName);
+                var result = query.Take(10);
+
+                foreach (var person in result)
+                {
+                    Console.WriteLine($"{person.FirstName} {person.LastName}");
+                }
+            }
+        }
+
+        static void BuildOptions()
+        {
+            _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+            _optionsBuilder = new DbContextOptionsBuilder<AdventureWorksContext>();
+            _optionsBuilder.UseSqlServer(_configuration.GetConnectionString("AdventureWorks"));
+        }
+    }
+}
+
+
